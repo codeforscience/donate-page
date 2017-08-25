9922c63a81c01cc3e628e9ecfd7b3863f084f708
Joe Hand <joe@hand.email>
2017-08-24 17:08:50 -0700

    wip: storing charges in hypercore

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+const path = require('path')
 const express = require('express')
 const Stripe = require('stripe')
 const bodyParser = require('body-parser')
+const StripeDat = require('stripe-dat')
 const config = require('./lib/config')
 
 const port = config.port || process.env.PORT
 const secretKey = config.stripe && config.stripe.keySecret ? config.stripe.keySecret : process.env.SECRET_KEY
 const publicKey = config.stripe && config.stripe.keyPublishable ? config.stripe.keyPublishable : process.env.PUBLISHABLE_KEY
+
+// Stripe Dat Config (stores donations in hypercore)
+const storage = path.join(__dirname, 'data') // TODO: config for server
+const startDate = new Date('1/1/2017')
 
 if (!secretKey) {
   throw new Error('Stripe secret key required')
@@ -13,6 +19,14 @@ if (!secretKey) {
 
 const app = express()
 const stripe = Stripe(secretKey)
+const sDat = StripeDat(secretKey, storage, {
+  filter: function (charge) {
+    if (charge.metadata && charge.metadata.donation) return true
+    return charge.description.indexOf('Donation to') > -1
+  },
+  anonymize: true,
+  fee: { pct: 0.022, amt: 0.3} // nonprofit fees =)
+})
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
@@ -22,6 +36,13 @@ app.get('/stripe-key', (req, res) => {
   // gotta be a better way to do this...
   // or just server side render js?
   res.json({ key: publicKey })
+})
+
+app.get('/donations', (req, res) => {
+  sDat.getCharges(startDate, function (err, charges) {
+    if (err) return res.status(500).send({error: err})
+    res.json(charges)
+  })
 })
 
 app.post('/charge', (req, res) => {
@@ -40,9 +61,18 @@ app.post('/charge', (req, res) => {
       description: 'Donation to Code for Science & Society',
       receipt_email: customer.email,
       currency: 'usd',
-      customer: customer.id
+      customer: customer.id,
+      metadata: {
+        'donation': true,
+        'source': 'donate.datproject.org'
+      }
     }))
-  .then(charge => res.send(charge))
+  .then(charge => {
+    sDat.addCharge(charge, function (err) {
+      if (err) console.error(err)
+      res.send(charge)
+    })
+  })
   .catch(err => {
     console.error('Stripe Processing Error:', err)
     if (err.type === 'StripeCardError') {
